8e8c637e6e43e0a76e8974e91229cdb206991869
maiki93 <fake@fake.com>
2021-10-14 09:12:26 +0200

    simple app is working, without modules

--- src/main/java/net/ddns/kimai/ExplorerSpring/ExplorerSpringApplication.java
+++ src/main/java/net/ddns/kimai/ExplorerSpring/ExplorerSpringApplication.java
@@ -1,13 +1,29 @@
 package net.ddns.kimai.ExplorerSpring;
 
+//import org.apache.logging.log4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExplorerSpringApplication {
+public class ExplorerSpringApplication  implements CommandLineRunner {
 
+	//private static Logger LOG = LoggerFactory.getLogger(ExplorerSpringApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ExplorerSpringApplication.class, args);
 	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Bonne chance aux aventuriers");
+		
+		
+		System.out.println("Relancez moi pour trouver encore plus de trésors");	
+	}
 
+		
+
+		
 }
