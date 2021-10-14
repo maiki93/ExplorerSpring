426594f7424a13f502f2301975264dfed87f7b24
maiki93 <fake@fake.com>
2021-10-14 08:43:51 +0200

    first commit

---
+++ src/main/java/net/ddns/kimai/ExplorerSpring/ExplorerSpringApplication.java
@@ -0,0 +1,13 @@
+package net.ddns.kimai.ExplorerSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ddns/kimai/ExplorerSpring/ExplorerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ddns.kimai.ExplorerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
